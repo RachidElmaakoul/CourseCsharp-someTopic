c3c5954b963412a105446673ce1b62efb17ccf90
RachidElmaakoul <146081594+RachidElmaakoul@users.noreply.github.com>
2024-06-26 01:15:09 +0100

    Add File Course Event

---
+++ Event/Event.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Event.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stock = new Stock("Amazon");
+
+            stock.Price = 100;
+
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+
+            stock.ChangeStockPriceBy(0.05m);
+            stock.ChangeStockPriceBy(-0.02m);
+            stock.ChangeStockPriceBy(0.0m);
+
+            Console.ReadKey();
+        }
+
+        private static void Stock_OnPriceChanged(Stock stock, decimal oldPrice)
+        {
+            string Result = "";
+            if (stock.Price > oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Result = "UP";
+            }else if (oldPrice > stock.Price)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Result = "Down";
+
+            }
+            else {
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            Console.WriteLine($" {stock.Name}  : {stock.Price} - {Result}");
+        }
+    }
+}
---
+++ Event/Event.ConsoleApp/Stock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event.ConsoleApp
+{
+    internal class Stock
+    {
+        public delegate void ChangeStockPriceHelperDelegate(Stock stock,decimal oldPrice);
+        public event ChangeStockPriceHelperDelegate OnPriceChanged;
+
+        private decimal _price;
+        private string _name = "";
+
+        public string Name => this._name;
+        public decimal Price { get => this._price; set => this._price = value; }
+
+        public Stock(string StockName)
+        {
+            this._name = StockName;
+        }
+        
+        public void ChangeStockPriceBy(decimal percent)
+        {
+            decimal OldPrice = this._price;
+            this._price += Math.Round(_price * percent, 2);
+
+            if (OnPriceChanged != null)
+            {
+                OnPriceChanged(this,OldPrice);
+            }
+
+        }
+       
+
+
+    }
+}
