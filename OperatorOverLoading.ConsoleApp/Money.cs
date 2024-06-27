2717d261b474fbfac324ae8fb4bfd6481f2cbdf7
RachidElmaakoul <146081594+RachidElmaakoul@users.noreply.github.com>
2024-06-28 00:30:57 +0100

    Add files via upload

---
+++ OperatorOverLoading.ConsoleApp/Money.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverLoading.ConsoleApp
+{
+    internal class Money
+    {
+
+        private decimal _amount;
+        public decimal Amount => _amount;
+
+
+        public Money(decimal value)
+        {
+            this._amount = value;
+        }
+
+        public static Money operator +(Money M1, Money M2){
+
+           var Value = M1._amount + M2._amount;
+            return new Money(Value);
+        }  
+        public static Money operator -(Money M1, Money M2){
+
+           var Value = M1._amount - M2._amount;
+            return new Money(Value);
+        } 
+        public static bool operator <(Money M1, Money M2){
+
+           return M1._amount < M2._amount;
+        }  public static bool operator >(Money M1, Money M2){
+
+           return M1._amount > M2._amount;
+        }  public static bool operator <=(Money M1, Money M2){
+
+           return M1._amount <= M2._amount;
+        }  public static bool operator >=(Money M1, Money M2){
+
+           return M1._amount >= M2._amount;
+        } 
+        public static Money operator ==(Money M1, Money M2){
+
+           var Value = M1._amount + M2._amount;
+            return new Money(Value);
+        }  public static Money operator !=(Money M1, Money M2){
+
+           var Value = M1._amount + M2._amount;
+            return new Money(Value);
+        } public static Money operator ++(Money M1){
+
+           var Value = M1._amount ;
+            return new Money(++Value);
+        }  public static Money operator --(Money M1){
+
+           var Value = M1._amount ;
+            return new Money(--Value);
+        }
+
+    }
+}
---
+++ OperatorOverLoading.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OperatorOverLoading.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var M1 = new Money(10);
+            var M2 = new Money(20);
+
+
+            var M3 = M1 + M2;
+            var M4 = M1 <= M2;
+
+            Console.WriteLine($" Amount : {M1.Amount}");
+            Console.WriteLine($" Amount after Add Methode OverLoading Add two class ({M1.Amount} + {M2.Amount}) : {M3.Amount}");
+            Console.WriteLine($"Is Money1 Less Than Money2 ({M1.Amount} <= {M2.Amount}) : {M4}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
