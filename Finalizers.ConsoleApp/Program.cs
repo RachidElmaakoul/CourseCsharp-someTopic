8e8cf7a4358a8148d1110ff57366c71fc2d4d2f5
RachidElmaakoul <146081594+RachidElmaakoul@users.noreply.github.com>
2024-06-28 15:19:49 +0100

    Add files via upload

---
+++ Finalizers.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Finalizers.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeSomeGarbage();
+            Console.WriteLine($"Memory befor call Gc Collector {GC.GetTotalMemory(false):N0}"); // number is right
+            //Console.WriteLine($"Memory befor call Gc Collector {GC.GetTotalMemory(false):N}"); here number is double
+            GC.Collect();
+
+            Console.WriteLine($"Memory After call Gc Collector {GC.GetTotalMemory(true):N0}");
+
+            Console.ReadKey();
+        }
+
+
+        static void MakeSomeGarbage()
+        {
+            Version v;
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version() ;
+            }
+        }
+
+    }
+}
