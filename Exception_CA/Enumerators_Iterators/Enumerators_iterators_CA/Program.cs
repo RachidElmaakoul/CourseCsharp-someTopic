37d80ba91ea3764d86726a6f30387cc1d0125ddc
RachidElmaakoul <146081594+RachidElmaakoul@users.noreply.github.com>
2024-08-19 00:57:51 +0100

    Add files via upload

---
+++ Exception_CA/Enumerators_Iterators/Enumerators_iterators_CA/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Enumerators_iterators_CA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Employye em1 = new Employye { ID = 123 , Name = "rachid el Maakoul", Salary = 1000m , Department = "IT"};
+          Employye em2 = new Employye { ID = 123 , Name = "rachid el Maakoul", Salary = 1000m , Department = "IT"};
+
+
+            Console.WriteLine(em1 == em2);
+            Console.WriteLine(em1.Equals(em2));
+            Console.ReadLine();
+        }
+    }
+    public class Employye
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
+
+        //Override Equals()
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Employye))
+                return false;
+
+
+            var emp = obj as Employye;
+            return this.ID == emp.ID
+                   && this.Name == emp.Name
+                   && this.Salary == emp.Salary
+                   && this.Department == emp.Department;
+        }
+
+
+        // Another Methode For Equals()
+        public static bool operator ==(Employye lho, Employye rho) => lho.Equals(rho);
+        public static bool operator !=(Employye lho, Employye rho) => !(lho.Equals(rho));
+
+        //Override GetHashCode()
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + ID.GetHashCode();
+            hash = hash * 23 + Name.GetHashCode();
+            hash = hash * 23 + Salary.GetHashCode();
+            hash = hash * 23 + Department.GetHashCode();
+            return hash;
+        }
+
+        //method 
+        //public static bool operator ==(Employye obj , Employye rho)
+        //{
+        //    var valueId = obj.ID == rho.ID;
+        //    var valueName = obj.Name == rho.Name;
+        //    var valueSalary = obj.Salary == rho.Salary;
+        //    var valueDepartment = obj.department == rho.department;
+        //    return (valueId && valueName && valueSalary && valueDepartment);
+        //}
+        //public static bool operator !=(Employye obj
+        //    , Employye rho)
+        //{
+        //    var valueId = obj.ID != rho.ID;
+        //    var valueName = obj.Name != rho.Name;
+        //    var valueSalary = obj.Salary != rho.Salary;
+        //    var valueDepartment = obj.department != rho.department;
+        //    return !(valueId && valueName && valueSalary && valueDepartment);
+        //}
+
+
+    }
+}
---
+++ Exception_CA/Enumerators_Iterators/Ienumerable_iterator_ex/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace Ienumerable_iterator_ex
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ints = new FiveIntegers( 1, 2, 3,4,5 );
+
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class FiveIntegers : IEnumerable
+    {
+        private int[] _value;
+
+        public FiveIntegers(int n1,int n2 , int n3, int n4 , int n5)
+        {
+            _value = new int[] {n1,n2,n3,n4,n5};
+        }
+
+        public IEnumerator GetEnumerator() => new Enumerator(this);
+        class Enumerator : IEnumerator
+        {
+            int CurrentIndex = -1;
+            FiveIntegers _integers;
+            public Enumerator(FiveIntegers integers)
+            {
+                _integers = integers;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (CurrentIndex == -1)
+                        throw new InvalidOperationException("Enumeration not started");
+                    if (CurrentIndex == _integers._value.Length)
+                        throw new InvalidOperationException("Enumeration has ended");
+
+                    return _integers._value[CurrentIndex];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (CurrentIndex >= _integers._value.Length - 1)
+                    return false;
+
+                return ++CurrentIndex < _integers._value.Length;
+            }
+
+            public void Reset() => CurrentIndex = -1;
+        }
+
+    }
+}
